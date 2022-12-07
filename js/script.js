0af265e8e2f0574a03ebe5b36b79e554d1935a96
leoaraujosa <leoo_araujoo@hotmail.com>
2022-12-07 03:29:16 -0300

    Added click functions to display answers in the frequently asked questions area.

--- js/script.js
+++ js/script.js
@@ -33,3 +33,23 @@ function ativarBikcraft(parametro) {
 
 parametros.forEach(ativarSeguro);
 parametros.forEach(ativarBikcraft);
+
+// Perguntas Frequentes
+
+const perguntas = document.querySelectorAll('.insurance-questions button');
+
+function ativarPergunta(event) {
+    const pergunta = event.currentTarget;
+    const atributo = pergunta.getAttribute('aria-controls');
+    const resposta = document.getElementById(atributo);
+    
+    resposta.classList.toggle('ativa'); // Inserir a classe "ativa" no botão que for clicado.
+    const ativa = resposta.classList.contains('ativa'); // Verifica se tem a classe ativa (true) ou se não tem (false).
+    pergunta.setAttribute('aria-expanded', ativa); // Setar o atribute aria-expanded como verdadeiro se a classe ativa existir ou como false se não existir.
+}
+
+function eventosPerguntas(pergunta) {
+    pergunta.addEventListener('click', ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
